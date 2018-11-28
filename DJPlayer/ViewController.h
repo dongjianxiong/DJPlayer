32b59be40759aacbe8eececee7484288802d8064
scad-build <scad-build@sohu-inc.com>
2018-11-28 16:37:52 +0800

    Initial Commit

---
+++ DJPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DJPlayer
+//
+//  Created by ioser on 2018/11/28.
+//  Copyright © 2018年 Lenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DJPlayerTests/DJPlayerTests.m
@@ -0,0 +1,39 @@
+//
+//  DJPlayerTests.m
+//  DJPlayerTests
+//
+//  Created by ioser on 2018/11/28.
+//  Copyright © 2018年 Lenny. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DJPlayerTests : XCTestCase
+
+@end
+
+@implementation DJPlayerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
